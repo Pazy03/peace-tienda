9e44c58697a508fc44986a767efe9d9edd5f55d3
Pazy03 <159201969+Pazy03@users.noreply.github.com>
2026-03-29 05:08:56 -0600

    se corrige producto java en domain

--- src/main/java/com/techShop/tienda/domain/Producto.java
+++ src/main/java/com/techShop/tienda/domain/Producto.java
@@ -1,6 +1,13 @@
 package com.techShop.tienda.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.Data;
 
@@ -20,11 +27,11 @@ public class Producto implements Serializable {
 
     private double precio;
 
+    @Column(name = "existencia")
     private int existencias;
 
     private boolean activo;
 
-    // Relación con Categoria
     @ManyToOne
     @JoinColumn(name = "id_categoria")
     private Categoria categoria;
